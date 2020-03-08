94c64dd15d9155f1be24ebaca4d64dc4b46bcfed
Ingimar <ingimareys93@gmail.com>
2020-03-08 10:49:41 +0100

    added comments

--- script.js
+++ script.js
@@ -1,38 +1,58 @@
-const magnifyIngGlass = document.querySelector("#circle");
-const clipCircle = document.querySelector("#clipcircle");
-const largeImage = document.querySelector(".largeImage");
-const largeImage2 = document.querySelector(".largeImage2");
+// get the svg element.
 const svg = document.querySelector("svg");
-
+// get the smallImage
+const smallimage = document.querySelector(".Sanfransisco");
+// add eventlistener and start the function
 svg.addEventListener("mousemove", moveCircle);
-// Native sizes is the sizes of the small image, so this can be
-let native_height = 1000;
-let native_width = 500;
 
 function moveCircle(event) {
+  // constants that We need to use in this function.
+  const magnifyIngGlass = document.querySelector("#circle");
+  const clipCircle = document.querySelector("#clipcircle");
+  const largeImage = document.querySelector(".largeImage");
+  const largeImage2 = document.querySelector(".largeImage2");
+
+  // Here i'm getting the position of the mouse on the svg relative to the document.
+  // so if youre in the top left corner of the image, it's 0 0, that way we can math out everything
+  // relative to the svg coordinates.
   var rect = this.getBoundingClientRect(),
     scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
+  // X and Y end up as the coordinates on the SVG image
   const X = event.pageX - (rect.left + scrollLeft);
   const Y = event.pageY - (rect.top + scrollTop);
+  // Native sizes is the sizes of the small image, so you could dynamically add and change image sizes.
+  let native_height = getimgHeight();
+  let native_width = getimgWidth();
 
-  if (X < 1000 && Y < 500 && X > 0 && Y > 0) {
+  // here is the start of the mgnifying function, I wanted to activate the glass when you go over the image and dissapear wehn you leave but
+  // the way the event listener is created it just stops calculating when youre off the image so this is always true in a sense (when over the image).
+  // Gonna look at this when I have time...
+  if (X < native_width && Y < native_height && X > 0 && Y > 0) {
     largeImage.classList.add("active");
   } else {
     largeImage.classList.remove("active");
   }
-
   if (largeImage.classList.contains("active")) {
-    const rx = Math.round((X / 1000) * native_width - 2000 / 2) * -1;
-    const ry = Math.round((Y / 500) * native_height - 1000 / 2) * -1;
-    const px = X;
-    const py = Y;
-    clipCircle.setAttribute("cx", px);
-    clipCircle.setAttribute("cy", py);
-    magnifyIngGlass.setAttribute("cx", px);
-    magnifyIngGlass.setAttribute("cy", py);
-    largeImage2.setAttribute("x", px * -1);
-    largeImage2.setAttribute("y", py * -1);
+    // here I'm setting the coordinates of the glass and cicles and the large image.
+    clipCircle.setAttribute("cx", X);
+    clipCircle.setAttribute("cy", Y);
+    magnifyIngGlass.setAttribute("cx", X);
+    magnifyIngGlass.setAttribute("cy", Y);
+    // litterally just tried out different calculations for this and stumbled on this one, so -1 is the key, as it will ofsett it so it's in the right location
+    //, so this function would tecnhically only work if the larger image is exactly 2x the size.
+    largeImage2.setAttribute("x", X * -1);
+    largeImage2.setAttribute("y", Y * -1);
   }
+}
+// here are some blackbox functions to get the widths and heights, would be so much easier with Jquery but whatever...
+function getimgWidth() {
+  const positionInfo = smallimage.getBoundingClientRect();
+  const width = positionInfo.width;
+  return width;
+}
+function getimgHeight() {
+  const positionInfo = smallimage.getBoundingClientRect();
+  const height = positionInfo.height;
+  return height;
 }
